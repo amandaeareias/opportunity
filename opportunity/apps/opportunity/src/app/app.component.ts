aa431200e974c08c814d3243e48f0547923bd169
Elena Tschimev <elena.m.obogeanu@gmail.com>
2019-03-16 00:02:59 +0100

    fix: data manipulation from db

--- opportunity/apps/opportunity/src/app/app.component.ts
+++ opportunity/apps/opportunity/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 // import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
-
+import {map} from 'rxjs/operators'
 import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'opp-root',
@@ -13,19 +13,27 @@ export class AppComponent {
   ngos$;
 
   constructor(private db: AngularFirestore) {
-    this.ngos$ = db.collection('ngo').valueChanges()
+    this.ngos$ = db.collection('ngo').snapshotChanges().pipe(map(actions => actions.map(action=> {
+     const data = action.payload.doc.data()
+     const id = action.payload.doc.id;
+     return {id, ...data}
+    })));
   }
 
-  update(item){
-    console.log(item, "i am the item")
-        this.db.collection('ngo').doc('WM8CPsEg223CeSCMLeit').delete().then(function() {
-          console.log("Document successfully deleted!");
-      }).catch(function(error) {
-          console.error("Error removing document: ", error);
-      });;
+  update(item) {
+    console.log(item, 'i am the item');
+    this.db
+      .collection('ngo')
+      .doc(item.id)
+      .delete()
+      .then(function() {
+        console.log('Document successfully deleted!');
+      })
+      .catch(function(error) {
+        console.error('Error removing document: ', error);
+      });
 
     // this.db.collection('ngo').doc(item.name).update(' UPDATED')
     // doc('ngo').update(item.name + ' worked')
   }
-
 }
