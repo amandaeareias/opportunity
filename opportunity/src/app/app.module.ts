bb3e46d6e410aa8418b1e2e91630a800356bdce6
Elena Tschimev <elena.m.obogeanu@gmail.com>
2019-03-16 18:00:22 +0100

    feat: add (pending) ngo signup form

--- opportunity/src/app/app.module.ts
+++ opportunity/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule , MatInputModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { LoginComponent } from './auth/login/login.component';
+import { NgoSignupComponent } from './ngo-signup/ngo-signup.component';
+import { VolunteerSignupComponent } from './volunteer-signup/volunteer-signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NgoSignupComponent,
+    VolunteerSignupComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +29,8 @@ import { LoginComponent } from './auth/login/login.component';
     AngularFireAuthModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
