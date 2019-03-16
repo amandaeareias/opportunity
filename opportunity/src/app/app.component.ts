19c81033ae037e0c7c5bbc48f5956655cda291a2
iiiked <igor@topfliteagency.com>
2019-03-16 12:52:11 +0100

    Add authentification with Google

--- opportunity/src/app/app.component.ts
+++ opportunity/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'opportunity';
+export class AppComponent implements OnInit {
+  user: any
+  constructor(
+    private auth: AngularFireAuth
+  ) {}
+
+  ngOnInit() {
+    this.auth.authState
+      .subscribe((user) => {
+        console.log(user);
+        this.user = user;
+        // console.log(isNgo);
+      });
+  }
 }
