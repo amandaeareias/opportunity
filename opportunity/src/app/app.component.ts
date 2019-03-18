c2f23dec9af07c10e829a572a238392a84aee6c5
Elena Tschimev <elena.m.obogeanu@gmail.com>
2019-03-18 23:35:28 +0100

    feat: add volunteer and ngo registration form

--- opportunity/src/app/app.component.ts
+++ opportunity/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { NgoSignupComponent } from './ngo-signup/ngo-signup.component';
+import { VolunteerSignupComponent } from './volunteer-signup/volunteer-signup.component';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,21 @@ import { NgoSignupComponent } from './ngo-signup/ngo-signup.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  fileNameDialogRef: MatDialogRef<NgoSignupComponent>;
-  private files = [
-    { name: 'foo.js', content: '' },
-    { name: 'bar.js', content: '' }
-  ];
 
   constructor(public dialog: MatDialog) {}
 
   // TODO: remove this code. Is for testing purpose only
   // this step will be included after the validation check for new users
-  openDialog(registrationData) {
+  openDialogNGO(registrationData) {
     this.dialog.open(NgoSignupComponent, {
+      data: {
+        filename: registrationData ? registrationData.name : ''
+      }
+    });
+  }
+
+  openDialogVolunteer(registrationData) {
+    this.dialog.open(VolunteerSignupComponent, {
       data: {
         filename: registrationData ? registrationData.name : ''
       }
--- opportunity/src/app/volunteer-signup/volunteer-signup.component.ts
+++ opportunity/src/app/volunteer-signup/volunteer-signup.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogRef
+} from '@angular/material';
 
 @Component({
   selector: 'app-volunteer-signup',
@@ -6,10 +13,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./volunteer-signup.component.css']
 })
 export class VolunteerSignupComponent implements OnInit {
+  isValid = true;
+  constructor(
+    private dialogRef: MatDialogRef<VolunteerSignupComponent>,
+    @Inject(MAT_DIALOG_DATA) private data
+  ) {}
 
-  constructor() { }
 
-  ngOnInit() {
+  volunteerProfile = new FormGroup({
+    nameForm: new FormControl('', [
+      Validators.minLength(2)
+    ]),
+    aboutForm: new FormControl('', [
+      Validators.minLength(20)
+    ]),
+    dateOfBirthForm: new FormControl('', [Validators.required])
+  });
+
+  ngOnInit() {}
+
+  close(): void {
+    this.dialogRef.close();
   }
 
+  submitVolunteer() {
+    const volunteerExtraFields = this.volunteerProfile.value;
+    const formResult = {
+      name: volunteerExtraFields.nameForm,
+      about: volunteerExtraFields.aboutForm,
+      dateOfBirth: volunteerExtraFields.dateOfBirthForm
+    };
+    console.log(formResult, 'user log');
+  }
 }
