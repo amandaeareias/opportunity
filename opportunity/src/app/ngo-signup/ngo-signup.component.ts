d4f46a1d2b2ab47a73100881ebaed2b248321949
Elena Tschimev <elena.m.obogeanu@gmail.com>
2019-03-18 18:22:58 +0100

    feat: implement validation errors chore: fix linting

--- opportunity/src/app/ngo-signup/ngo-signup.component.ts
+++ opportunity/src/app/ngo-signup/ngo-signup.component.ts
@@ -1,45 +1,46 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { CdkTextareaAutosize } from "@angular/cdk/text-field";
-import { NGO } from "../models/ngo.model";
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { NGO } from '../models/ngo.model';
 import {
   MatDialog,
   MAT_DIALOG_DATA,
   MatDialogTitle,
   MatDialogRef
-} from "@angular/material";
+} from '@angular/material';
 
 @Component({
-  selector: "app-ngo-signup",
-  templateUrl: "./ngo-signup.component.html",
-  styleUrls: ["./ngo-signup.component.css"]
+  selector: 'app-ngo-signup',
+  templateUrl: './ngo-signup.component.html',
+  styleUrls: ['./ngo-signup.component.css']
 })
 export class NgoSignupComponent implements OnInit {
+  isValid = true;
   constructor(
     private dialogRef: MatDialogRef<NgoSignupComponent>,
     @Inject(MAT_DIALOG_DATA) private data
   ) {}
 
   ngoProfile = new FormGroup({
-    orgNameForm: new FormControl("",[Validators.required,Validators.minLength(2)]),
-    descriptionForm: new FormControl("",[Validators.required, Validators.minLength(100)]),
-    websiteForm: new FormControl("",[Validators.required]),
-    addressForm: new FormControl("",[Validators.required]),
-    emailForm: new FormControl("",[Validators.required, Validators.email]),
-    phoneForm: new FormControl("", [Validators.required])
+    orgNameForm: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    descriptionForm: new FormControl('', [Validators.required, Validators.minLength(100)]),
+    websiteForm: new FormControl('', [Validators.required]),
+    addressForm: new FormControl('', [Validators.required]),
+    emailForm: new FormControl('', [Validators.required, Validators.email]),
+    phoneForm: new FormControl('', [Validators.required])
   });
 
   ngOnInit() {}
 
   get fields() { return this.ngoProfile.controls; }
 
-  validateAllFormFields(formGroup: FormGroup) {         //{1}
-  Object.keys(formGroup.controls).forEach(field => {  //{2}
-    const control = formGroup.get(field);             //{3}
-    if (control instanceof FormControl) {             //{4}
+  validateAllFormFields(formGroup: FormGroup) {
+  Object.keys(formGroup.controls).forEach(field => {
+    const control = formGroup.get(field);
+    if (control instanceof FormControl) {
       control.markAsTouched({ onlySelf: true });
-    } else if (control instanceof FormGroup) {        //{5}
-      this.validateAllFormFields(control);            //{6}
+    } else if (control instanceof FormGroup) {
+      this.validateAllFormFields(control);
     }
   });
 }
@@ -61,6 +62,7 @@ export class NgoSignupComponent implements OnInit {
       console.log('form submitted');
     } else {
       this.validateAllFormFields(this.ngoProfile);
+      this.isValid = false;
     }
     // this.dialogRef.close(`${this.ngoProfile.value}`);
   }
