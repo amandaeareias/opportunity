1ea57132a2a6020312bb87036a2c9a27f87aba90
Elena Tschimev <elena.m.obogeanu@gmail.com>
2019-03-19 12:25:12 +0100

    chore: fix linting issues

--- opportunity/src/app/google-maps/google-maps.component.ts
+++ opportunity/src/app/google-maps/google-maps.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-google-maps",
-  templateUrl: "./google-maps.component.html",
-  styleUrls: ["./google-maps.component.css"]
+  selector: 'app-google-maps',
+  templateUrl: './google-maps.component.html',
+  styleUrls: ['./google-maps.component.css']
 })
 export class GoogleMapsComponent implements OnInit {
-  lat: number = 41.3851;
-  lng: number = 2.1734;
+  lat = 41.3851;
+  lng = 2.1734;
   disableStyles = [
     {
       featureType: 'poi',
