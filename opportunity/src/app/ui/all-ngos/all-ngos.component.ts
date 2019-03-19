8652424308bffb4d7fbee53b3069718eb27b56e3
Amanda Areias <amanda.1108@hotmail.com>
2019-03-19 12:42:06 +0100

    created allOpportunities and allNGOs pages and improved some navigation

---
+++ opportunity/src/app/ui/all-ngos/all-ngos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-ngos',
+  templateUrl: './all-ngos.component.html',
+  styleUrls: ['./all-ngos.component.css']
+})
+export class AllNgosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
