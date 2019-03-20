4eb046ec08db3ad5bbd08e4d45d6cfaae3bd9a99
iiiked <igor@topfliteagency.com>
2019-03-20 12:25:39 +0100

    Implement mapping service

---
+++ opportunity/src/app/data/services/mapping.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MappingService {
+
+  constructor() { }
+
+  mapVolunteerInputToProps(id, { name, about, dateOfBirth }) {
+    return [
+      id,
+      {
+        name,
+        about,
+        dateOfBirth,
+      }
+    ];
+  }
+
+  mapNgoInputToProps(id, { name, about, website, address, email, phone }) {
+    return [
+      id,
+      {
+        name,
+        about,
+        contact: {
+          website,
+          address,
+          publicEmail: email,
+          phone,
+        }
+      }
+    ];
+  }
+
+  /* mapOpportunityInputToProps is ONLY used for creating Opportunity record */
+  /* USE updateOpportunity from firebase.service directly for updating Opportunity */
+  mapOpportunityInputToProps(ngoData, { name, about, location, prerequisites }) {
+    return {
+      name,
+      about,
+      location,
+      prerequisites,
+      ngo: {
+        id: ngoData.id,
+        name: ngoData.name,
+        image: ngoData.image,
+      },
+      timeCreated: new Date().toISOString(),
+      active: true,
+    }
+  }
+}
