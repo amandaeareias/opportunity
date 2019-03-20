d50c9ab33ddd69a42079a87964a27fddff855a92
Amanda Areias <amanda.1108@hotmail.com>
2019-03-20 19:15:30 +0100

    started implementing volunteer application

--- opportunity/src/app/ui/ngo-profile/opportunity/opportunity.component.ts
+++ opportunity/src/app/ui/ngo-profile/opportunity/opportunity.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import { FirebaseCrudService } from '../../../data/services/firebase.service'
+import { MappingService } from '../../../data/services/mapping.service'
 import { Store } from '@ngrx/store'
 import { userDetailsSelector, isUserNgoSelector } from '../../../user/user.reducers'
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-opportunity',
@@ -12,12 +14,19 @@ import { userDetailsSelector, isUserNgoSelector } from '../../../user/user.reduc
 export class OpportunityComponent implements OnInit {
 
   @Input()dialogConfig;
+  currentUser;
+  applying: boolean = false
+
+  applyForm = new FormGroup({
+    apply: new FormControl('', [Validators.required, Validators.minLength(10)])
+  });
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public opportunity,
     private fbService: FirebaseCrudService,
     private store: Store<any>,
-    private dialog: MatDialogRef<OpportunityComponent>
+    private dialog: MatDialogRef<OpportunityComponent>,
+    private mappingService: MappingService,
   ) { }
 
   ngOnInit() {
@@ -31,6 +40,7 @@ export class OpportunityComponent implements OnInit {
       })
     this.store.select(userDetailsSelector)
       .subscribe(user => {
+        this.currentUser = user
         this.applyOpp(this.opportunity, user, isNgo)
       })
   }
@@ -43,9 +53,19 @@ export class OpportunityComponent implements OnInit {
       console.log('please log-in')
       this.dialog.close()
     } else {
-      console.log('applying')
+      this.applying = true
     }
+  }
 
+  formSubmit() {
+    if(this.applyForm.valid){
+      console.log('waiting for the function to be ready, uncomment next 2 lines when it is')
+      //const data = this.mappingService.mapApplicationInputToProps(this.currentUser.id, this.opportunity.id, this.applyForm.value.apply)
+      // this.fbService.createApplication(data)
+      this.dialog.close()
+    }else {
+      console.log('not valid')
+    }
   }
 
 }
