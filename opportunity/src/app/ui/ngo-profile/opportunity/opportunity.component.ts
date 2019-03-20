ef273803b5f2a0219471d729669d5f04f4af4895
Amanda Areias <amanda.1108@hotmail.com>
2019-03-20 18:10:40 +0100

    check if user can apply to opportunity

--- opportunity/src/app/ui/ngo-profile/opportunity/opportunity.component.ts
+++ opportunity/src/app/ui/ngo-profile/opportunity/opportunity.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import { inject } from '@angular/core/testing';
-
+import { FirebaseCrudService } from '../../../data/services/firebase.service'
+import { Store } from '@ngrx/store'
+import { userDetailsSelector, isUserNgoSelector } from '../../../user/user.reducers'
 
 @Component({
   selector: 'app-opportunity',
@@ -13,12 +14,38 @@ export class OpportunityComponent implements OnInit {
   @Input()dialogConfig;
 
   constructor(
-    private dialogRef: MatDialogRef<OpportunityComponent>,
-    @Inject(MAT_DIALOG_DATA) public opportunity
+    @Inject(MAT_DIALOG_DATA) public opportunity,
+    private fbService: FirebaseCrudService,
+    private store: Store<any>,
+    private dialog: MatDialogRef<OpportunityComponent>
   ) { }
 
   ngOnInit() {
-    console.log('xx', this.opportunity)
+  }
+
+  applyClicked() {
+    let isNgo: boolean;
+    this.store.select(isUserNgoSelector)
+      .subscribe(result => {
+        isNgo = result
+      })
+    this.store.select(userDetailsSelector)
+      .subscribe(user => {
+        this.applyOpp(this.opportunity, user, isNgo)
+      })
+  }
+
+  applyOpp(opportunity, user, isNgo: boolean) {
+    if(isNgo) {
+      console.log('only volunteers can apply')
+      this.dialog.close()
+    } else if (!user.id) {
+      console.log('please log-in')
+      this.dialog.close()
+    } else {
+      console.log('applying')
+    }
+
   }
 
 }
