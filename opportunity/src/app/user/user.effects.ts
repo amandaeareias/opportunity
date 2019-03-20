388d807e8c958b6c3a27ba9a9c88c8fd1be4d1d3
iiiked <igor@topfliteagency.com>
2019-03-20 10:17:26 +0100

    Apply changes requested by @Elena

--- opportunity/src/app/user/user.effects.ts
+++ opportunity/src/app/user/user.effects.ts
@@ -4,7 +4,7 @@ import { EMPTY, of } from 'rxjs';
 import { map, switchMap, catchError, first } from 'rxjs/operators';
 
 import { ActionTypes, LoadUserDetails } from './user.actions';
-import { FirebaseCrudService } from '../data/services/firebase.service';
+import { LoginService } from './user-auth/login.service';
 
 @Injectable()
 export class UserEffects {
@@ -16,22 +16,25 @@ export class UserEffects {
       /* TODO: Implement db.getUserData$. Using a mock instead */
       switchMap(({ payload }) => {
         const { logInEmail, photoURL, displayName, isNgo } = payload;
-        return this.db.registerUser({
+        return this.auth.registerUser({
           logInEmail,
           photoURL,
           displayName,
         }, isNgo)
+        /* Pipe result of registerUser to get only the first response */
+        /* This fixes a bug of double-response triggered by real-time DB */
+        /* @TODO: Add catchError logic instead of returning EMPTY */
           .pipe(
             first(),
             map(({ user, isNgo }) => new LoadUserDetails({ user, isNgo })),
-            catchError(() => EMPTY)
-          )
+            catchError(() => EMPTY),
+          );
       })
     );
 
   constructor(
     private actions$: Actions,
-    private db: FirebaseCrudService
+    private auth: LoginService,
   ) {}
 
 }
