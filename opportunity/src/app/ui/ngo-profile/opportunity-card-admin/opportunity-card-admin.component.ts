9926973906941675fd97d1a15d01e922387f794e
Amanda Areias <amanda.1108@hotmail.com>
2019-03-20 17:36:31 +0100

    fixed deleted file

---
+++ opportunity/src/app/ui/ngo-profile/opportunity-card-admin/opportunity-card-admin.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunity-card-admin',
+  templateUrl: './opportunity-card-admin.component.html',
+  styleUrls: ['./opportunity-card-admin.component.css']
+})
+export class OpportunityCardAdminComponent implements OnInit {
+
+  @Input() opportunity;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
