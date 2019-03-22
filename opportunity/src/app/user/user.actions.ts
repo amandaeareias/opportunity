38fc40fea1ae3cd8dd316070796e4ed252d9c252
Igor Snitkin <djshked@gmail.com>
2019-03-22 08:59:12 +0100

    Clear up code in ngrx part a little bit

--- opportunity/src/app/user/user.actions.ts
+++ opportunity/src/app/user/user.actions.ts
@@ -1,29 +1,30 @@
 import { Action } from '@ngrx/store';
+
 import { Volunteer } from '../data/models/volunteer.model';
 import { NGO } from '../data/models/ngo.model';
 
 export enum ActionTypes {
-  LoginWithGoogle_SUCCESS = '[User] Authed in with Google',
-  LoginWithGoogle_FAIL = '[User] Authentification failed',
-  Logout = '[User] Log user out',
-  FetchUserDetails = '[User] Request user info from DB',
-  LoadUserDetails = '[User] Load information about user from DB',
+  GOOGLE_LOGIN_SUCCESS = '[User] Authed in with Google',
+  GOOGLE_LOGIN_FAILURE = '[User] Authentification failed',
+  USER_LOGOUT = '[User] Log user out',
+  GET_USER_PENDING = '[User] Request user info from DB',
+  GET_USER_SUCCESS = '[User] Load user info from DB to state',
 }
 
-export class LoginWithGoogle_SUCCESS implements Action {
-  readonly type = ActionTypes.LoginWithGoogle_SUCCESS;
+export class GOOGLE_LOGIN_SUCCESS implements Action {
+  readonly type = ActionTypes.GOOGLE_LOGIN_SUCCESS;
 }
 
-export class LoginWithGoogle_FAIL implements Action {
-  readonly type = ActionTypes.LoginWithGoogle_FAIL;
+export class GOOGLE_LOGIN_FAILURE implements Action {
+  readonly type = ActionTypes.GOOGLE_LOGIN_FAILURE;
 }
 
-export class Logout implements Action {
-  readonly type = ActionTypes.Logout;
+export class USER_LOGOUT implements Action {
+  readonly type = ActionTypes.USER_LOGOUT;
 }
 
-export class FetchUserDetails implements Action {
-  readonly type = ActionTypes.FetchUserDetails;
+export class GET_USER_PENDING implements Action {
+  readonly type = ActionTypes.GET_USER_PENDING;
 
   constructor(public payload: {
     isNgo: boolean,
@@ -33,14 +34,14 @@ export class FetchUserDetails implements Action {
   }) {}
 }
 
-export class LoadUserDetails implements Action {
-  readonly type = ActionTypes.LoadUserDetails;
+export class GET_USER_SUCCESS implements Action {
+  readonly type = ActionTypes.GET_USER_SUCCESS;
 
   constructor(public payload: { user: Volunteer | NGO , isNgo: boolean }) {}
 }
 
-export type UserActions = LoginWithGoogle_SUCCESS
-  | LoginWithGoogle_FAIL
-  | Logout
-  | FetchUserDetails
-  | LoadUserDetails;
+export type UserActions = GOOGLE_LOGIN_SUCCESS
+  | GOOGLE_LOGIN_FAILURE
+  | USER_LOGOUT
+  | GET_USER_PENDING
+  | GET_USER_SUCCESS;
