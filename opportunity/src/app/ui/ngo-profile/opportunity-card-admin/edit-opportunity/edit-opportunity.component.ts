aec3a956434e6c8edf004623483382a301ea7c7c
Amanda Areias <amanda.1108@hotmail.com>
2019-03-23 10:31:35 +0100

    implementing edit

--- opportunity/src/app/ui/ngo-profile/opportunity-card-admin/edit-opportunity/edit-opportunity.component.ts
+++ opportunity/src/app/ui/ngo-profile/opportunity-card-admin/edit-opportunity/edit-opportunity.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {MatSnackBar} from '@angular/material';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar, MatChipInputEvent } from '@angular/material';
 import { SnackbarComponent } from '../../../snackbar/snackbar.component'
 import { FirebaseCrudService } from '../../../../data/services/firebase.service'
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-edit-opportunity',
@@ -16,8 +17,14 @@ export class EditOpportunityComponent implements OnInit {
     name: new FormControl(this.opportunity.name, Validators.required),
     about: new FormControl(this.opportunity.about, Validators.required),
     location: new FormControl(this.opportunity.location, Validators.required),
-    prerequisites: new FormControl(this.opportunity.prerequisites),
   });
+
+  prerequisitesList: string[] = this.opportunity.prerequisites;
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public opportunity,
@@ -27,11 +34,16 @@ export class EditOpportunityComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
   }
 
   formSubmit() {
-    if(this.updateOpportunityForm.valid) {
-      console.log('implement update opportunity function/db')
+    if (this.updateOpportunityForm.valid) {
+      let newOpp = {
+        ...this.updateOpportunityForm.value,
+        prerequisites: this.prerequisitesList
+      }
+      this.fbService.updateOpportunity(this.opportunity.id, newOpp)
       this.snackBar.openFromComponent(SnackbarComponent, {
         duration: 3000,
       });
@@ -46,9 +58,27 @@ export class EditOpportunityComponent implements OnInit {
   deleteOpportunity() {
     console.log(this.opportunity)
     let confirmation = confirm("Are you sure you want to delete this opportunity?");
-    if(confirmation) {
+    if (confirmation) {
       this.fbService.deleteOpportunity(this.opportunity.id, this.opportunity.ngo.id)
       this.dialog.close()
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+    if ((value || '').trim()) {
+      this.prerequisitesList.push(value.trim());
+    }
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(value: string): void {
+    const index = this.prerequisitesList.indexOf(value);
+    if (index >= 0) {
+      this.prerequisitesList.splice(index, 1);
     }
   }
 
