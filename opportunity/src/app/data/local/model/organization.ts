21d0459aeb3983f1af1948b07202da8722dd513f
Igor Snitkin <djshked@gmail.com>
2019-03-24 22:32:01 +0100

    Add some models

--- opportunity/src/app/data/local/model/organization.ts
+++ opportunity/src/app/data/local/model/organization.ts
@@ -1,7 +1,19 @@
 import { ContactPoint } from './contactPoint';
-import { Event } from './event';
+import { Location } from './location';
+import { Review } from './review';
 
 export class Organization {
+  category: string;
   contact: ContactPoint[];
+  description: string;
   event: string[]; // IDs
+  featuredImage: string;
+  legalName: string;
+  logo: string;
+  name: string;
+  primaryLocation: Location;
+  rating: number;
+  review: Review[];
+  timeCreated: string;
+  timePublished: string;
 }
