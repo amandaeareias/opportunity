ec8c61bad17771ea0039e3e7557b17e56bc61c3a
Amanda Areias <amanda.1108@hotmail.com>
2019-03-24 10:47:31 +0100

    user can update profile picture

--- opportunity/src/app/ui/navbar/settings/settings-volunteer/settings-volunteer.component.ts
+++ opportunity/src/app/ui/navbar/settings/settings-volunteer/settings-volunteer.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FirebaseCrudService } from '../../../../data/services/firebase.service'
-import {MatSnackBar} from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import { SnackbarComponent } from '../../../snackbar/snackbar.component'
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,6 +15,10 @@ import { SnackbarComponent } from '../../../snackbar/snackbar.component'
   styleUrls: ['./settings-volunteer.component.css']
 })
 export class SettingsVolunteerComponent implements OnInit {
+
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  selectedImage: string = this.currentUser.user.image
 
   settingsForm = new FormGroup({
     name: new FormControl(this.currentUser.user.name, Validators.required),
@@ -23,15 +30,22 @@ export class SettingsVolunteerComponent implements OnInit {
     private fbService: FirebaseCrudService,
     private dialog: MatDialogRef<SettingsVolunteerComponent>,
     private snackBar: MatSnackBar,
+    private storage: AngularFireStorage,
   ) { }
 
   ngOnInit() {
     console.log(this.currentUser)
   }
 
+
   formSubmit() {
     if (this.settingsForm.valid) {
-      this.fbService.updateVolunteer(this.currentUser.user.id, this.settingsForm.value)
+      let newData = {
+        name: this.settingsForm.value.name,
+        about: this.settingsForm.value.about,
+        image: this.selectedImage
+      }
+      this.fbService.updateVolunteer(this.currentUser.user.id, newData)
         .then(res => {
           this.snackBar.openFromComponent(SnackbarComponent, {
             duration: 3000,
@@ -40,6 +54,30 @@ export class SettingsVolunteerComponent implements OnInit {
         })
         .catch(e => console.log('Not possible to submit, error: ', e))
     }
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const filePath = file.name.split('.')[0] + '-' + Date.now() + '.' + file.name.split('.')[1];
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        this.downloadURL = fileRef.getDownloadURL()
+        this.downloadURL.subscribe(link => {
+          this.selectedImage = link
+        })
+      })
+    )
+      .subscribe()
+  }
+
+  cancel() {
+    this.dialog.close()
   }
 
   deleteProfile() {
