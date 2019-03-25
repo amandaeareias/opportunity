6a3001b348549bb0a6b1f93de8f922c3ea9634a5
Amanda Areias <amanda.1108@hotmail.com>
2019-03-25 13:14:10 +0100

    implemented category on ngo and search by category

--- opportunity/src/app/data/models/ngo.model.ts
+++ opportunity/src/app/data/models/ngo.model.ts
@@ -7,6 +7,7 @@ export class NGO {
   image?: string;
   about?: string;
   rating?: number;
+  category: string;
   contact?: {
     website?: string;
     address: string;
--- opportunity/src/app/ui/homepage/ngolist/ngolist.component.ts
+++ opportunity/src/app/ui/homepage/ngolist/ngolist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FirebaseCrudService } from '../../../data/services/firebase.service'
+import { categoriesList } from '../../../data/listsofdata/categorieslist'
 
 @Component({
   selector: 'app-ngolist',
@@ -9,16 +10,23 @@ import { FirebaseCrudService } from '../../../data/services/firebase.service'
 export class NgolistComponent implements OnInit {
 
   ngos;
+  categoriesList: string[] = categoriesList
 
   constructor(
     private service: FirebaseCrudService,
-  ) {
-    this.service.getMany('ngos').subscribe(ngos => {
-      this.ngos = ngos;
-    })
-   }
+  ) { }
 
   ngOnInit() {
+    this.getNgos()
+  }
+
+  getNgos() {
+    this.service.getMany('ngos').subscribe(ngos => this.ngos = ngos)
+  }
+
+  changeone(event) {
+    let target = event.target.value
+    target === 'All' ? this.getNgos() : this.service.getMany('ngos', (ref) => ref.where('category', '==', target)).subscribe(ngos => this.ngos = ngos)
   }
 
 }
